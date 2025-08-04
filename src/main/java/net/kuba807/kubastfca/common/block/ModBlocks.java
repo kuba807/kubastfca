bd501798494bbfe0934eccf2d5ae6a274fc2f1ac
kuba807 <kuba807@op.pl>
2025-08-04 19:47:51 +0200

    started working on pickable tea fixed loot table etc added black tea

--- src/main/java/net/kuba807/kubastfca/common/block/ModBlocks.java
+++ src/main/java/net/kuba807/kubastfca/common/block/ModBlocks.java
@@ -51,6 +51,9 @@ public class ModBlocks {
 
     public static final RegistryObject<LiquidBlock> TEA_GREEN_BLOCK = BLOCKS.register("tea_green_block",
             () -> new LiquidBlock(ModFluids.SOURCE_TEA_GREEN ,BlockBehaviour.Properties.copy(Blocks.WATER)));
+
+    public static final RegistryObject<LiquidBlock> TEA_BLACK_BLOCK = BLOCKS.register("tea_black_block",
+            () -> new LiquidBlock(ModFluids.SOURCE_TEA_BLACK ,BlockBehaviour.Properties.copy(Blocks.WATER)));
         //uprawy
         public static final Map<Crop, RegistryObject<Block>> CROPS = Helpers.mapOfKeys(Crop.class, crop ->
                 BLOCKS.register("crop/" + crop.getSerializedName(), crop::create));
